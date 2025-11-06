954843089b4e7272f07867c9c29ed22e58295d00
Hato1125 <hato.flavius@gmail.com>
2025-11-07 00:47:37 +0900

    chore: setup cmake

---
+++ src/ttfs.hh
@@ -0,0 +1,12 @@
+#ifndef _TTFS_TTFS_HH
+#define _TTFS_TTFS_HH
+
+#include <cstdint>
+
+namespace ttfs {
+  constexpr std::uint8_t major = 1u;
+  constexpr std::uint8_t minor = 0u;
+  constexpr std::uint8_t patch = 0u;
+}
+
+#endif
